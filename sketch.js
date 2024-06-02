bbd29651a8d0cea8461d7c73419ab37200284f6c
Otávio <152627627+profotavio@users.noreply.github.com>
2024-06-02 00:18:22 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,45 @@
+let xBolinha = 300;
+let yBolinha = 160;
+let diametro = 15;
+let raio = diametro / 2;
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  mostraRaquete();
+  
+function mostraBolinha(){
+  circle(xBolinha, yBolinha, diametro)
+}
+
+function mostraRaquete() {
+    rect(xRaquete, yRaquete, raqueteComprimento, raqueteAltura);
+}
+
+function movimentaBolinha(){
+  xBolinha += velocidadeXBolinha
+  yBolinha += velocidadeYBolinha  
+}
+function   verificaColisaoBorda(){
+if (xBolinha + raio > width || xBolinha - raio < 0){
+  velocidadeXBolinha *= -1
+}
+if (yBolinha + raio > height || yBolinha - raio < 0){ 
+  velocidadeYBolinha *= -1
+}
+}
+  }
